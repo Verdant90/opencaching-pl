792f6cc7aefb9ec3ed53ae7143f0468b02f14028
wloczynutka <wloczynutka@gmail.com>
2016-05-09 10:38:49 +0200

    add graphics for medal REGION_MAZOWIECKIE

--- lib/Controllers/config/medals.config.php
+++ lib/Controllers/config/medals.config.php
@@ -711,8 +711,77 @@ return array(
                 ),
             ),
         ),
-    ), /* end of medal */
-
+    ), /* end of medal REGION_LODZKIE*/
+ MedalsContainer::REGION_MAZOWIECKIE => array(
+        'name' => _('regionMazowieckie'),
+        'description' => _('Medal for geocaching in Mazowieckie'),
+        'type' => MedalsController::MEDAL_TYPE_REGION,
+        'dateIntroduced' => '2006-04-09 10:30:00',
+        'conditions' => array(
+            'cacheType' => array(
+                GeoCache::TYPE_TRADITIONAL,
+                GeoCache::TYPE_MULTICACHE,
+                GeoCache::TYPE_QUIZ,
+                GeoCache::TYPE_OTHERTYPE,
+            ),
+            'cacheLocation' => array(
+                'code3' => 'PL11',
+            ),
+            'minimumAltitude' => false,
+            'cacheCountToAward' => array(
+                1 => array(
+                    'levelName' => _('mazowieckie1'),
+                    'cacheCount' => array(
+                        'found' => 1,
+                        'placed' => 0,
+                    ),
+                ),
+                2 => array(
+                    'levelName' => _('mazowieckie2'),
+                    'cacheCount' => array(
+                        'found' => 15,
+                        'placed' => 0,
+                    ),
+                ),
+                3 => array(
+                    'levelName' => _('mazowieckie3'),
+                    'cacheCount' => array(
+                        'found' => 33,
+                        'placed' => 0,
+                    ),
+                ),
+                4 => array(
+                    'levelName' => _('mazowieckie4'),
+                    'cacheCount' => array(
+                        'found' => 75,
+                        'placed' => 1,
+                    ),
+                ),
+                5 => array(
+                    'levelName' => _('mazowieckie5'),
+                    'cacheCount' => array(
+                        'found' => 155,
+                        'placed' => 2,
+                    ),
+                ),
+                6 => array(
+                    'levelName' => _('mazowieckie6'),
+                    'cacheCount' => array(
+                        'found' => 333,
+                        'placed' => 5,
+                    ),
+                ),
+                7 => array(
+                    'levelName' => _('mazowieckie7'),
+                    'cacheCount' => array(
+                        'found' => 555,
+                        'placed' => 10,
+                    ),
+                ),
+            ),
+        ),
+    ), /* end of medal REGION_MAZOWIECKIE*/
+    
     MedalsContainer::CACHE_TRADITIONAL => array(
         'name' => _('TraditionalCache'),
         'description' => _('Medal for traditional caches activity'),
